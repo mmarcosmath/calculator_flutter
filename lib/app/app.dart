6da11c77e30c8a63b3f700c26860d0378c7862b3
mmarcosmath <mmarcos@acad.ifma.edu.br>
2021-01-05 22:44:06 -0300

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'screens/calculator.dart';
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
